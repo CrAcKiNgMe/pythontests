f70a1af4e0909355b0b9a48957917b58d407ff10
CrAcKiNgMe <sunyong19890@163.com>
2017-01-24 11:48:55 +0800

    01 implement a Stack

--- b_tree.py
+++ b_tree.py
@@ -1,61 +1,35 @@
-import os
-import Queue
+#No. 02 - implement a Stack
 
+class Stack:
+     def __init__(self):
+         self.items = []
 
-class Node:
+     def isEmpty(self):
+         return self.items == []
 
-    def __init__(self,value):
-      self.left = None
-      self.right = None
-      self.value = value
+     def push(self, item):
+         self.items.append(item)
 
-    def getLeftChild(self):
-        return self.left
-    def getRightChild(self):
-        return self.right
-    def setNodeValue(self,value):
-        self.rootid = value
-    def getNodeValue(self):
-        return self.rootid
-    def pre_order(self):
-        print self.value
-        if(self.left):self.left.pre_order()
-        if(self.right):self.right.pre_order()
-    def in_order(self):
-        if(self.left):self.left.in_order()
-        print self.value
-        if(self.right):self.right.in_order()
-    def post_order(self):
-        if(self.left):self.left.post_order()
-        if(self.right):self.right.post_order()
-        print self.value
+     def pop(self):
+         return self.items.pop()
 
-    def level_order(self):
-        q = Queue.Queue()
-        q.put(self);
-        while (q.qsize() > 0):
-            tmp = q.get()
-            print tmp.value
-            if(tmp.left): q.put(tmp.left)
-            if(tmp.right): q.put(tmp.right)
+     def top(self):
+         return self.items[self.size()-1]
+     def size(self):
+         return len(self.items)
 
+a = Stack()
 
+a.push(4)
+a.push(5)
+a.push(6)
+a.push(7)
 
-
-btree = Node("A")
-btree.left = Node("B")
-btree.right = Node("C")
-btree.left.left = Node("D")
-btree.left.right = Node("E")
-btree.right.left   = Node("F")
-#btree.right.right  = Node("6")
-#btree.right.right.left  = Node("7")
-
-
-btree.level_order()
-
-
-
-
-
-
+print a.size()
+print a.top()
+print a.top()
+print a.pop()
+print a.pop()
+print a.pop()
+print a.pop()
+print a.size()
