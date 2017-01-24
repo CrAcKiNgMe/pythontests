64dcb4c923abce943d3ff4c426b01f59be3be2df
CrAcKiNgMe <sunyong19890@163.com>
2017-01-24 16:55:44 +0800

    No. 03 - Maximum Sum of All Sub-arrays[http://codercareer.blogspot.jp/2011/09/no-03-maximum-sum-of-all-sub-arrays.html]

--- b_tree.py
+++ b_tree.py
@@ -1,82 +1,23 @@
-#No. 02 - implement a Stack wit minimize number
-
-class Stack:
-     def __init__(self):
-         self.items = []
-         self.min_items = []
-
-     def isEmpty(self):
-         return self.items == []
-
-     def push(self, item):
-         if(self.size() == 0):
-             self.min_items.append(item)
-         elif(item > self.min()):
-             self.min_items.append(self.min())
-         else:
-             self.min_items.append(item)
-         self.items.append(item)
-     def pop(self):
-         self.min_items.pop()
-         return self.items.pop()
-
-     def top(self):
-         return self.items[self.size()-1]
-
-     def min(self):
-         return self.min_items[len(self.min_items) - 1]
-
-     def size(self):
-         return len(self.items)
+#No. 03 - Maximum Sum of All Sub-arrays
+"""
+If we have a deep understanding of algorithm, we may solve this problem with dynamic programming.
+If function f(i) stands for the maximum sum of a sum-array ended with the ith number,
+ what it is to get is max[f(i)].
+"""
 
 
-class Stack2:
-    def __init__(self):
-        self.items = []
-    def size(self):
-        return len(self.items)
-    def top(self):
-        return self.items[len(self.items) - 1]
-
-    def push(self, item):
-        if(self.size() == 0):
-            self.items.append(item)
-            self.min  = item
-        elif(item > self.min):
-            self.items.append(item)
-        else:
-            self.items.append(2 * item - self.min)
-            self.min = item
-    def pop(self):
-        if(self.min < self.top()):
-            return self.items.pop()
-        else:
-            tmp = self.min
-            self.min = self.min * 2 - self.top()
-            self.items.pop()
-            return  tmp
-
-    def minnum(self):
-        return self.min
-
-    def content(self):
-        print self.items
-
-s = Stack2()
-
-s.push(88)
-s.push(89)
-s.push(90)
-s.push(1)
+a = list([1, -2, 3, 10, -4, 7, 2, -5])
 
 
 
-print s.content(), s.minnum()
-s.pop()
-print s.content(), s.minnum()
-s.pop()
-print s.content(), s.minnum()
-s.pop()
-print s.content(), s.minnum()
-s.pop()
 
+
+for i in range(len(a) - 1):
+    if(a[i] > 0):
+        a[i+1] += a[i]
+
+for i in range(len(a) - 1):
+    if(a[i+1] > a[0]):
+        a[0] = a[i+1]
+
+print a[0]
