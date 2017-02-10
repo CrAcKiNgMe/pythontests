6ef3ad7672d07c1a468a7cf7b8ee6d03da955d72
CrAcKiNgMe <sunyong19890@163.com>
2017-02-10 16:40:45 +0800

    min cost path[http://www.geeksforgeeks.org/dynamic-programming-set-6-min-cost-path/]

--- b_tree.py
+++ b_tree.py
@@ -1,56 +1,47 @@
 # -*- coding: utf-8 -*-
+#desc
+#http://www.geeksforgeeks.org/dynamic-programming-set-6-min-cost-path/
 """
-Dynamic Programming | Set 5 (Edit Distance)
-Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+Given a cost matrix cost[][] and a position (m, n) in cost[][], write a function that returns cost of minimum cost path to reach (m, n) from (0, 0). Each cell of the matrix represents a cost to traverse through that cell. Total cost of a path to reach (m, n) is sum of all the costs on that path (including both source and destination). You can only traverse down, right and diagonally lower cells from a given cell, i.e., from a given cell (i, j), cells (i+1, j), (i, j+1) and (i+1, j+1) can be traversed. You may assume that all costs are positive integers.
 
-Insert
-Remove
-Replace
+For example, in the following figure, what is the minimum cost path to (2, 2)?
 
-What are the subproblems in this case?
-The idea is process all characters one by one staring from either from left or right sides of both strings.
-Let we traverse from right corner, there are two possibilities for every pair of character being traversed.
+1) Optimal Substructure
+The path to reach (m, n) must be through one of the 3 cells: (m-1, n-1) or (m-1, n) or (m, n-1). So minimum cost to reach (m, n) can be written as “minimum of the 3 cells plus cost[m][n]”.
 
-m: Length of str1 (first string)
-n: Length of str2 (second string)
-If last characters of two strings are same, nothing much to do. Ignore last characters and get count for remaining strings. So we recur for lengths m-1 and n-1.
-Else (If last characters are not same), we consider all operations on ‘str1’, consider all three operations on last character of first string, recursively compute minimum cost for all three operations and take minimum of three values.
-Insert: Recur for m and n-1
-Remove: Recur for m-1 and n
-Replace: Recur for m-1 and n-1
-
+minCost(m, n) = min (minCost(m-1, n-1), minCost(m-1, n), minCost(m, n-1)) + cost[m][n]
 """
 
-# A Naive recursive Python program to fin minimum number
-# operations to convert str1 to str2
-def editDistance(str1, str2, m , n):
+list = [ [1, 2, 3], [4, 8, 2], [1, 5, 3]]
 
-    # If first string is empty, the only option is to
-    # insert all characters of second string into first
-    if m==0:
-         return n
+"""
+[1, 2, 3]
+[4, 8, 2]
+[1, 5, 3]
+"""
 
-    # If second string is empty, the only option is to
-    # remove all characters of first string
-    if n==0:
-        return m
 
-    # If last characters of two strings are same, nothing
-    # much to do. Ignore last characters and get count for
-    # remaining strings.
-    if str1[m-1]==str2[n-1]:
-        return editDistance(str1,str2,m-1,n-1)
+def min(a, b, c):
+    m = a
+    if(a > b):
+        m = b
+    if(m > c):
+        m = c
+    return m
 
-    # If last characters are not same, consider all three
-    # operations on last character of first string, recursively
-    # compute minimum cost for all three operations and take
-    # minimum of three values.
-    return 1 + min(editDistance(str1, str2, m, n-1),    # Insert
-                   editDistance(str1, str2, m-1, n),    # Remove
-                   editDistance(str1, str2, m-1, n-1)    # Replace
-                   )
 
-# Driver program to test the above function
-str1 = "sunday"
-str2 = "saturday"
-print editDistance(str1, str2, len(str1), len(str2))
+
+
+def minpath(matrix, m, n):
+
+    if(m == 1 and n == 1):
+        return matrix[0][0]
+    if(m < 1 or n < 1):
+        return 999999999999999999999999
+    else:
+        n =  min(minpath(matrix, m-1, n-1),minpath(matrix, m, n-1),minpath(matrix, m-1, n)) + matrix[m-1][n-1]
+
+        return n
+
+
+print minpath(list, 3 , 3)
