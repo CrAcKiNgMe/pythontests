e8f7d5b72345f60d0202f0184524476cc3b7198f
CrAcKiNgMe <sunyong19890@163.com>
2017-02-10 11:55:07 +0800

    LIS[http://www.geeksforgeeks.org/dynamic-programming-set-3-longest-increasing-subsequence/]

--- b_tree.py
+++ b_tree.py
@@ -1,40 +1,39 @@
-
-#Dynamic Programming | Set 10 ( 0-1 Knapsack Problem)
-
-a = [-1] * 5 * 6
-
-def KnapSack(W, wt, val, n):
-    if(n==0 or W==0):
-        return 0
-    if(wt[n-1] > W):
-        return KnapSack(W,wt, val, n-1)
-    return max(KnapSack(W,wt, val, n-1),
-               val[n-1] + KnapSack(W-wt[n-1],wt, val, n-1))
+# Longest Increasing Subsequence
+"""
+Optimal Substructure:
+Let arr[0..n-1] be the input array and L(i) be the length of the LIS ending at index i such that arr[i] is the last element of the LIS.
+Then, L(i) can be recursively written as:
+L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
+L(i) = 1, if no such j exists.
+To find the LIS for a given array, we need to return max(L(i)) where 0 < i < n.
+Thus, we see the LIS problem satisfies the optimal substructure property as the main problem can be solved using solutions to subproblems.
+"""
 
 
-def knapsack(W, wt, val, n):
-    K = [[0 for x in range(W+1)] for x in range(n+1)]
-    for i in range(n+1):
-        for w in range(W+1):
-            if(i == 0 or w ==0):
-                K[i][w] = 0
-            elif wt[i-1] <= w:
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
-            else:
-                K[i][w] = K[i-1][w]
-
-    return K[n][W]
+list = [62,92, 96, 43, 28, 37, 92, 5, 3, 54, 93, 83, 22]
+listlen = len(list)
 
 
+memlist = [1]*listlen#memlist[i] means   the length of the LIS ending at index i
 
 
+def lic(a, n):
+    global memlist
+    if n==1:
+        return 1
+    lic_ = 1
 
+    for i in range(n - 1):
+        if(memlist[i]> 1):
+            subproblem_lis_length = memlist[i]
+        else:
+            subproblem_lis_length = lic(a, i+1)
+        if(a[n-1] > a[i] and lic_ < subproblem_lis_length+1):
+            lic_ = subproblem_lis_length + 1
+            memlist[n-1]=lic_
 
+    return lic_
 
+print lic(list, listlen)
+print memlist
 
-
-val = [60, 100, 120]
-wt = [10, 20, 30]
-W = 50
-n = len(val)
-print knapsack(W , wt , val , n)
