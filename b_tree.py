4413720b70d9009b02794387d16d7d60a98fa86f
CrAcKiNgMe <sunyong19890@163.com>
2017-02-10 15:25:37 +0800

    lcs[http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/]

--- b_tree.py
+++ b_tree.py
@@ -1,39 +1,41 @@
-# Longest Increasing Subsequence
 """
-Optimal Substructure:
-Let arr[0..n-1] be the input array and L(i) be the length of the LIS ending at index i such that arr[i] is the last element of the LIS.
-Then, L(i) can be recursively written as:
-L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
-L(i) = 1, if no such j exists.
-To find the LIS for a given array, we need to return max(L(i)) where 0 < i < n.
-Thus, we see the LIS problem satisfies the optimal substructure property as the main problem can be solved using solutions to subproblems.
+Dynamic Programming | Set 4 (Longest Common Subsequence)
+We have discussed Overlapping Subproblems and Optimal Substructure properties in Set 1 and Set 2 respectively. We also discussed one example problem in Set 3. Let us discuss Longest Common Subsequence (LCS) problem as one more example problem that can be solved using Dynamic Programming.
+
+LCS Problem Statement: Given two sequences, find the length of longest subsequence present in both of them. A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. For example, "abc", "abg", "bdf", "aeg", "acefg", .. etc are subsequences of "abcdefg". So a string of length n has 2^n different possible subsequences.
+
+
+
+
+It is a classic computer science problem, the basis of diff (a file comparison program that outputs the differences between two files), and has applications in bioinformatics.
+
+Examples:
+LCS for input Sequences "ABCDGH" and "AEDFHR" is "ADH" of length 3.
+LCS for input Sequences "AGGTAB" and "GXTXAYB" is "GTAB" of length 4.
+
+
+1) Optimal Substructure:
+Let the input sequences be X[0..m-1] and Y[0..n-1] of lengths m and n respectively. And let L(X[0..m-1], Y[0..n-1]) be the length of LCS of the two sequences X and Y. Following is the recursive definition of L(X[0..m-1], Y[0..n-1]).
+
+If last characters of both sequences match (or X[m-1] == Y[n-1]) then
+L(X[0..m-1], Y[0..n-1]) = 1 + L(X[0..m-2], Y[0..n-2])
+
+If last characters of both sequences do not match (or X[m-1] != Y[n-1]) then
+L(X[0..m-1], Y[0..n-1]) = MAX ( L(X[0..m-2], Y[0..n-1]), L(X[0..m-1], Y[0..n-2])
 """
 
 
-list = [62,92, 96, 43, 28, 37, 92, 5, 3, 54, 93, 83, 22]
-listlen = len(list)
+list1 = list("123abc")
+list2 = list("abc123abc")
 
 
-memlist = [1]*listlen#memlist[i] means   the length of the LIS ending at index i
+def lcs(X, m, Y, n):
+    if(m==0 or n == 0):
+        return 0
+    if(X[m-1] == Y[n-1]):
+        return 1 + lcs(X, m-1, Y, n-1)
+    else:
+        return max(lcs(X, m-1, Y, n), lcs(X, m, Y, n-1))
 
 
-def lic(a, n):
-    global memlist
-    if n==1:
-        return 1
-    lic_ = 1
-
-    for i in range(n - 1):
-        if(memlist[i]> 1):
-            subproblem_lis_length = memlist[i]
-        else:
-            subproblem_lis_length = lic(a, i+1)
-        if(a[n-1] > a[i] and lic_ < subproblem_lis_length+1):
-            lic_ = subproblem_lis_length + 1
-            memlist[n-1]=lic_
-
-    return lic_
-
-print lic(list, listlen)
-print memlist
-
+print lcs(list1, len(list1), list2, len(list2))
