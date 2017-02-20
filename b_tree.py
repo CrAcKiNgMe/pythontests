7ff701a6b950bd2447e6b985d48d540c630fe70d
CrAcKiNgMe <sunyong19890@163.com>
2017-02-20 19:05:39 +0800

    add stoppable thread

--- b_tree.py
+++ b_tree.py
@@ -14,6 +14,7 @@ import threading
 import os
 
 import timeit
+import datetime
 
 
 
@@ -27,29 +28,37 @@ import time
 exitFlag = 0
 
 class mythread(threading.Thread):
-    def __init__(self, target = None, interval = 0.01,delay = 0, counter = 0):
+    def __init__(self, target = None,args = None, interval = 0.01,delay = 0, counter = 0):
         threading.Thread.__init__(self)
         self.interval = interval
         self.delay    = delay
         self.counter  = counter
         self.target  = target
+        self.arg     = args
+        self.runFlag = 1
+    def end(self):
+        self.runFlag = 0
+
+
+
     def run(self):
-        print timeit.timeit("""
-        if(self.delay > 0):
-            time.sleep(self.delay)
-        if(self.counter == 0):
-            while 1:
-                if(self.target):
-                    self.target()
-                if(self.interval > 0):
-                    time.sleep(self.interval)
-        else:
-            while self.counter:
-                if(self.target):
-                    self.target()
-                if(self.interval > 0):
-                    time.sleep(self.interval)
-                self.counter = self.counter - 1""")
+        def runwithtimer():
+            if(self.delay > 0):
+                time.sleep(self.delay)
+            if(self.counter == 0):
+                while 1 and self.runFlag:
+                    if(self.target):
+                        self.target(self)
+                    if(self.interval > 0):
+                        time.sleep(self.interval)
+            else:
+                while self.counter and self.runFlag:
+                    if(self.target):
+                        self.target(self.arg)
+                    if(self.interval > 0):
+                        time.sleep(self.interval)
+                    self.counter = self.counter - 1
+        runwithtimer()
 
 
 
@@ -58,37 +67,23 @@ class mythread(threading.Thread):
 
 
 class MyPool:
-    def print_time(self):
+    def print_time(self, threadobj):
 
-
-            header = {"user-agent": "my-app/0.0.1"}
-            r = requests.get("http://httpbin.org/get?asdf=asdf", headers=header)
-            #r= requests.get("http://bbs.hupu.com/")#, headers=header)
-            #print u"thread{0},response{1}".format(threading._get_ident(), r.headers)
-            self.lock.acquire()
-            self.tickcnt += 1
-            if(self.tickcnt > 100000):
-                self.lock.release()
-                return
-
-
-
-            con = sqlite3.connect("C:/abc/test.db", check_same_thread=False)
-            with con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO test VALUES(\
-        1,\
-        'sadf',\
-        1234,\
-        CURRENT_TIMESTAMP\
-        )")
-                res = cur.fetchall()
-
-            if con:
-                con.commit()
-                con.close()
-
+        header = {"user-agent": "my-app/0.0.1"}
+        # r = requests.get("http://httpbin.org/get?asdf=asdf", headers=header)
+        # r= requests.get("http://bbs.hupu.com/")#, headers=header)
+        # print u"thread{0},response{1}".format(threading._get_ident(), r.headers)
+        self.lock.acquire()
+        self.tickcnt += 1
+        print self.tickcnt
+        if (self.tickcnt > 100000):
             self.lock.release()
+            threadobj.end()
+            return
+        tmp = (self.tickcnt,"str", random.random(), str(datetime.datetime.now()))
+
+        self.data.append(tmp)
+        self.lock.release()
 
 
     def __init__(self, cnt = 20, maxcnt = 50):
@@ -97,43 +92,61 @@ class MyPool:
         self.threads = [None]*cnt
         self.lock    = threading.Lock()
         self.tickcnt = 0
+        self.data = []
 
-        con = sqlite3.connect("C:/abc/test.db", check_same_thread=False)
-        with con:
-            cur = con.cursor()
+
+
+        self.con = sqlite3.connect("C:/abc/test.db", check_same_thread=False)
+        with self.con:
+            cur = self.con.cursor()
             cur.execute("create table if not exists test(\
             A interger,\
             B vchar(256),\
             C numeric(256,3),\
             D interger\
             )")
-            res = cur.fetchall()
 
-        if con:
-            con.commit()
-            con.close()
-
-
-
-
-
+        if self.con:
+            self.con.commit()
         for i in range(self.cnt):
-            self.threads[i] = mythread(target=self.print_time, interval=0);
+            self.threads[i] = mythread(target=self.print_time,args=None, interval=0);
+
+
+
 
     def start(self):
-
         for i in range(self.cnt):
             self.threads[i].start()
+    def join(self):
         for i in range(self.cnt):
             self.threads[i].join()
 
+    def finalize(self):
+        with self.con:
+            cur = self.con.cursor()
+            cur.executemany("""INSERT INTO test
+             VALUES(?, ?, ?, ?)""",self.data)
+
+        if self.con:
+            self.con.commit()
+            self.con.close()
 
 
 
 
 
-pool = MyPool(1, 100)
-#pool.start()
+
+
+pool = MyPool(100, 100)
+pool.start()
+pool.join()
+pool.finalize()
+
+
+
+
+
+
 
 
 
