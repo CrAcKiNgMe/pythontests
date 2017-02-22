5fe0721ecc9bfb2cfcaeaed127319900a61666e2
CrAcKiNgMe <sunyong19890@163.com>
2017-02-22 18:56:08 +0800

    中缀转后缀

--- test.py
+++ test.py
@@ -1,109 +1,48 @@
+n = raw_input()
+n = int(n)
 
-import os
+dic = {"+":0,"-":0,"*":1,"/":1,"(":2, ")":3}
+exps = []
 
-import threading
-import  Queue
-
-q = Queue.Queue()
-
-q.put(1234,False)
-q.put(2,False)
-q.put(3,False)
-q.put(4,False)
-q.put(5,False)
-
-print q.qsize()
-print q.get(False)
-print q.get(False)
-print q.get(False)
-print q.get(False)
-print q.get(False)
-
-print q.qsize()
-
-def foo():
-    a = 5
-    if(a > 5):
-        print "a>5"
-        print "foo**********************************"
-    else:
-        print "a <= 5";
-        print "hahha"
-
-a = [1,2,3,4,5]
-
-print "********************"
-for iter in a:
-    if(iter == 1):
-        a.remove(3)
-    print iter
-
-print "********************"
-
-class base():
-    pass
-
-a = base()
+for i in range(n):
+    rawdata = raw_input()
+    rawdata = filter(None, rawdata)
+    exps.append(rawdata)
 
 
-from anytree import Node, RenderTree
-udo = Node("Udo")
-marc = Node("Marc", parent=udo)
-lian = Node("Lian", parent=marc)
-dan = Node("Dan", parent=udo)
-jet = Node("Jet", parent=dan)
-jan = Node("Jan", parent=dan)
-joe = Node("Joe", parent=dan)
-
-print udo
-
-for pre, fill, node in RenderTree(udo):
-    print("%s%s" % (pre, node.name))
-
-seq_a = list("helao")#list
-seq_a2 = [1,2,3,5]  #list
+def printexp(exp):
+    tmpstack = []
+    for i in range(len(exp)):
+        if(exp[i]<= 'z' and exp[i] >= 'a'):
+            print exp[i]
+        elif(exp[i] == '('):
+            tmpstack.append(exp[i])
+        elif(exp[i] == ')'):
+            while(len(tmpstack) and tmpstack[len(tmpstack) - 1] != '('):
+                print tmpstack.pop()
+            if (len(tmpstack)):
+                tmpstack.pop()
+        elif(len(tmpstack) == 0 or dic[exp[i]] > dic[tmpstack[len(tmpstack)-1]]):
+            tmpstack.append(exp[i])
+        else:
+            while(len(tmpstack) and dic[exp[i]] <= dic[tmpstack[len(tmpstack)-1]]):
+                if(dic[tmpstack[len(tmpstack)-1]] != '('):
+                    print tmpstack.pop()
+            tmpstack.append(exp[i])
 
 
 
 
-seq_b = "heelo"#str
-seq_b2 = str("12345")#str
-
-seq_b3=seq_b.upper();
-
-print seq_b3
-
-seq_c = unicode("134")#unicode
-seq_c2 = unicode(seq_b2)
-
-
-seq_d = tuple('abc') #(tuple[1,2,3])
-seq_d2 = tuple([1,3,4])
-
-
-seq_e = bytearray(seq_d)
 
 
 
-print type(seq_a)
-
-print type(seq_a2)
-print type(seq_b)
-print type(seq_b2)
-print type(seq_c2)
-print seq_c2
-print type(seq_d)
-print seq_d
-print seq_d2
-
-print type(seq_e)
 
 
-d = xrange(0, 10,2)
-print type(d)
-print d
 
 
-e = range(0, 10,2)
-print type(e)
-print e
+
+for i in range(n):
+    printexp(exps[i])
+
+
+
